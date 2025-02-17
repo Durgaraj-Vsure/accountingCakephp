0c0bf3a83ca88beecc7e5582699913b8ec73f0e0
Durgaraj <durgaraj.chauhan@vsureconsultancy.com>
2025-02-17 18:09:14 +0530

    first commit

---
+++ app/Config/webzash.php
@@ -0,0 +1,11 @@
+<?php
+	$wz['datasource'] = 'Database/Mysql';
+	$wz['database'] = 'webzash';
+	$wz['host'] = 'localhost';
+	$wz['port'] = '3306';
+	$wz['login'] = 'root';
+	$wz['password'] = 'pass';
+	$wz['prefix'] = '';
+	$wz['encoding'] = 'utf8';
+	$wz['persistent'] = '';
+?>
